67d665e2f69fb3a2f55d26e321e39a91ddf9a483
v-namratha <58245045+v-namratha@users.noreply.github.com>
2022-12-29 07:20:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,7 @@ string = "Startup's Profit Prediction"
 st.set_page_config(page_title=string, page_icon="✅", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 st.title (string, anchor=None)
-st.write("""
 
-            - By *Vedant Mukhedkar* :sunglasses: 
-
-""")
 
 
 from PIL import Image
